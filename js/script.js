7d55187bbc99bc73f6a0bba532c1e4a893df29a6
Leshista <a.leshista@gmail.com>
2024-03-24 12:28:43 +0300

    Form without adaptive done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-// Обрабатываем нажатие на пункт в навигации, меняем то, какой пункт активный
-document.querySelectorAll('.header-navigation-list__item').forEach((e) => {
-    e.addEventListener('click', () => {
-        document
-            .querySelectorAll('.header-navigation-list__item')
-            .forEach((item) => {
+// Обрабатываем нажатие на пункт, меняем то, какой пункт активный
+function activeHandler(selector) {
+    document.querySelectorAll(selector).forEach((e) => {
+        e.addEventListener('click', () => {
+            document.querySelectorAll(selector).forEach((item) => {
                 item.classList.contains('active')
                     ? item.classList.remove('active')
                     : null;
             });
-        e.classList.toggle('active');
+            e.classList.toggle('active');
+        });
     });
-});
+}
+activeHandler('.header-navigation-list__item');
+activeHandler('.system-select__item');
+
+var slider = document.querySelector('.order-form-slider');
+var output = document.querySelector('.order-form-slider-counter');
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
 
 // Скролл к форме по нажатию на якоря, не оставляет мусора в url
 document.querySelectorAll("[scroll='goScrollToForm']").forEach((link) => {
